505e13d179978b9671b4e7e492a9d9760dd88ab5
xoxokwenniex <160082716+xoxokwenniex@users.noreply.github.com>
2026-01-05 19:42:01 +0800

    theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+ import { useState, useEffect } from "react";
 import { Button } from "./components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
 import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
